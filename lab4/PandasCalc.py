0e49343f5412657ce724537f122774459d5b1331
tramon <andriitramon@gmail.com>
2025-04-02 17:11:57 +0300

    lab-04 - 4.2.3

--- lab4/PandasCalc.py
+++ lab4/PandasCalc.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     df = pd.read_csv("bank.csv")
-    print('Description:\n', df.describe(include="all"))
+    print('Description:\n', df.describe())
 
     print('\nInfo:')
     df.info()
@@ -15,8 +15,20 @@ if __name__ == '__main__':
     fig, axes = plt.subplots(2, 1, figsize=(10, 10))
 
     for i, col in enumerate(numeric_fields):
-        axes[i].hist(df[col], bins=30, edgecolor='black')
+        axes[i].hist(df[col], bins=30, edgecolor='grey')
         axes[i].set_title(f'Histogram: {col}')
+        axes[i].set_xlabel(col)
+        axes[i].set_ylabel('Amount')
+
+    plt.tight_layout()
+    plt.show()
+
+    nominal_fields = ['marital', 'deposit']
+    fig, axes = plt.subplots(2, 1, figsize=(10, 10))
+
+    for i, col in enumerate(nominal_fields):
+        df[col].value_counts().plot(color='orange', kind='bar', ax=axes[i], edgecolor='grey')
+        axes[i].set_title(f'Distribution: {col}')
         axes[i].set_xlabel(col)
         axes[i].set_ylabel('Amount')
 
